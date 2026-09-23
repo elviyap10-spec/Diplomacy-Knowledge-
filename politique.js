5745dcd8f4ce26f133acea9c48091f2eeb2319db
Reinapirata229 <332603780+elviyap10-spec@users.noreply.github.com>
2026-09-23 15:41:41 +0000

    Update politique.js

--- politique.js
+++ politique.js
@@ -2,4 +2,21 @@
 // DIPLOMACY LAB — POLITIQUE
 // ========================================
 
+const politicalData = {
+
+    CIV: {
+
+        officialName: "République de Côte d'Ivoire",
+
+        capital: "Yamoussoukro",
+
+        regime: "Présidentiel"
+
+    }
+
+};
+
+
 console.log("Module POLITIQUE chargé.");
+
+console.log(politicalData);
